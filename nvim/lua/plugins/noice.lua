7d8de11d68685dd8d38e3dfdbdf860965d70918c
Dylan Hawkins <dylan@swellmadeit.com>
2024-11-30 00:57:56 +0200

    Some updates

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,16 +1,17 @@
 
 return {
   "folke/noice.nvim",
-  config = function()
-    require("noice").setup({
-      cmdline = {
-        view = "cmdline_popup", -- используем всплывающее окно для командной строки
-        format = {
-          cmdline = { position = { row = "50%", col = "50%" } }, -- размещаем по центру экрана
-        },
-      },
-    })
-  end,
+  enabled = false
+  -- config = function()
+  --   require("noice").setup({
+  --     cmdline = {
+  --       view = "cmdline_popup", -- используем всплывающее окно для командной строки
+  --       format = {
+  --         cmdline = { position = { row = "50%", col = "50%" } }, -- размещаем по центру экрана
+  --       },
+  --     },
+  --   })
+  -- end,
   -- dependencies = {
   --   "MunifTanjim/nui.nvim", -- зависимость для noice.nvim
   --   "rcarriga/nvim-notify", -- опционально для улучшенных уведомлений
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -69,6 +69,11 @@ return {
       -- LSP Server Settings
       ---@type lspconfig.options
       servers = {
+        eslint = {
+          settings = {
+            workingDirectory = { mode = "auto" }
+          },
+        },
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
           -- Use this to add any additional keymaps
